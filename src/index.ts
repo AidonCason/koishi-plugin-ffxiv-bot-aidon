ff3df9e7667b66b13ee30363d9c6c2c8146d3d58
aidon <silverblade999@outlook.com>
2024-04-10 16:03:31 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'ffxiv-bot-aidon'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.command('查询 <item_name>')
+    .action((err, item_name) => {
+      console.log(item_name);
+      return item_name;
+    });
 }

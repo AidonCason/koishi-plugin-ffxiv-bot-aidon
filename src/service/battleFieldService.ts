1849a57f451cb92bad2176b72f54c2b5d29b1014
aidon <silverblade999@outlook.com>
2024-05-07 13:36:52 +0800

    Revert "try fix"
    This reverts commit 4f0f79fb649c6f824bde8d61d0d99d91e009c6ef.

--- src/service/battleFieldService.ts
+++ src/service/battleFieldService.ts
@@ -18,7 +18,6 @@ const getBattleFieldInfoHandler = (
   if (config.use_markdown_qq && config.templdate_id_pvp_qq && argv.session.qq) {
     const data = {
       msg_type: 2, // 2 markdown
-      content: '',
       markdown: {
         custom_template_id: config.templdate_id_pvp_qq,
         params: [
